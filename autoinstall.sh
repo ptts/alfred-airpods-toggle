bbbbab461b913804a6e592d9e9ee1fae52112735
Patrick <patrick.steffen@me.com>
2019-03-23 17:20:06 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update / Upgrade Homebrew
 /usr/local/bin/brew update
 /usr/local/bin/brew upgrade
+
+# Install required packages
 brew install bluetoothconnector
 brew install switchaudio-osx
-cd $TMPDIR && curl -OL "https://github.com/ptts/alfred-airpods/raw/master/AirPods.alfredworkflow"
-cd $TMPDIR && open -a "/Applications/Alfred 3.app" "$TMPDIR"AirPods.alfredworkflow
-# cd $TMPDIR && rm AirPods.alfredworkflow
+
+# Download and install workflow
+cd $TMPDIR && curl -OL "https://github.com/ptts/alfred-airpods/raw/master/AirPods.alfredworkflow" && open -a "/Applications/Alfred 3.app" "$TMPDIR"AirPods.alfredworkflow
+
+# DONE
 echo "All done."

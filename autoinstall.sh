c8baa7379d21249f50f33afa35334e951ad9b23b
Patrick <patrick.steffen@me.com>
2019-03-26 11:46:51 +0100

    made smarter

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,40 +1,85 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Install Homebrew
-if [[ $(command -v brew) == "" ]]; then
-        echo "[...] Installing Homebrew.. "
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+function _exists {
+    if [[ $(command -v "$1") == "" ]]; then
+        return 1
     else
-        echo "[✓] Homebrew already installed. Updating.. "
-        brew update
-        brew upgrade
+        return 0
+    fi;
+}
+
+function log() { # $1 = string
+    COLOR='\033[1;33m'
+    NC='\033[0m'
+    printf "${COLOR}$1${NC}\n"
+}
+
+function log_success() { # $1 = string
+    COLOR='\033[0;32m'
+    NC='\033[0m'
+    printf "${COLOR}$1${NC}\n"
+}
+
+function log_error() { # $1 = string
+    COLOR="\033[0;31m"
+    NC='\033[0m'
+    printf "${COLOR}$1${NC}\n"
+}
+
+if _exists brew; then
+    log_success "[✓] Homebrew already installed. Updating.. "
+    brew update
+    brew upgrade
+else
+    log "[...] Installing Homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [ $? -eq 0 ]; then
+        log_success "[✓] Homebrew successfully installed."
+    else
+        log_error "[X] Homebrew installation failed. Abort."
+        exit 1
+    fi
 fi;
 
-# Install requirements
-if [[ $(command -v bluetoothconnector) == "" ]]; then
-        echo "[...] Installing bluetoothconnector.. "
-        brew install bluetoothconnector
+if _exists bluetoothconnector; then
+    log_success "[✓] bluetoothconnector already installed."
+else
+    log "[...] Installing bluetoothconnector"
+    brew install bluetoothconnector
+    if [ $? -eq 0 ]; then
+        log_success "[✓] bluetoothconnector successfully installed."
     else
-        echo "[✓] bluetoothconnector already installed."
+        log_error "[X] bluetoothconnector installation failed. Abort."
+        exit 1
+    fi
 fi;
 
-if [[ $(command -v SwitchAudioSource) == "" ]]; then
-        echo "[...] Installing switchaudio-osx.. "
-        brew install switchaudio-osx
-        echo "[✓] switchaudio-osx installed"
+if _exists SwitchAudioSource; then
+    log_success "[✓] switchaudio-osx already installed"
+else
+    log "[...] Installing switchaudio-osx"
+    brew install switchaudio-osx
+    if [ $? -eq 0 ]; then
+        log_success "[✓] switchaudio-osx successfully installed."
     else
-        echo "[✓] switchaudio-osx already installed"
+        log_error "[X] switchaudio-osx installation failed. Abort."
+        exit 1
+    fi
 fi;
 
-# Install workflow
-if [[ ! $(command -v brew) == "" ]] && [[ ! $(command -v bluetoothconnector) == "" ]] && [[ ! $(command -v SwitchAudioSource) == "" ]]; then
-    echo "[✓] All requirements installed"
-    echo "[...] Installing workflow..."
+if _exists SwitchAudioSource && _exists SwitchAudioSource && _exists SwitchAudioSource; then
+    log_success "[✓] All requirements installed"
+    log "[...] Installing workflow"
     cd $TMPDIR
     curl -OLs "https://github.com/ptts/alfred-airpods/raw/master/AirPods.alfredworkflow" && open -a "/Applications/Alfred 3.app" "$TMPDIR"AirPods.alfredworkflow
-    echo "[✓] DONE. Workflow installed"
+    if [ $? -eq 0 ]; then
+        log_success "[✓] AirPods Workflow successfully installed."
+        log_success "[✓] ALL DONE"
+    else
+        log_error "[X] AirPods Workflow installation failed. Abort."
+        exit 1
+    fi
 else
-    echo "[X] Installation failed. Requirements not found."
+    log_error "[X] Installation failed. Requirements not found."
 fi;
-
 
